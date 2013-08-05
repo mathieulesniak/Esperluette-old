bc1cfc921607ff57e32eda02611f4adca42ce5cd
Mathieu LESNIAK <mathieu@lesniak.fr>
2013-08-05 21:50:02 +0200

    [New] Default theme

---
+++ themes/default/post.php
@@ -0,0 +1,14 @@
+<?php themeInclude('header'); ?>
+
+<section class="post">
+    <h1>Article TITLE</h1>
+    <article>
+        ZE POST
+    </article>
+</section>
+
+<section class="comments">
+    ZE COMMENTS
+</section>
+
+<?php themeInclude('footer'); ?>
