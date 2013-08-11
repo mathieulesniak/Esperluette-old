9ecbacf8e39496aa6119d78af492d93757cb7dcd
Mathieu LESNIAK <mathieu@lesniak.fr>
2013-08-11 15:27:55 +0200

    [Upg] Notification support

--- Esperluette/Model/Notification.php
+++ Esperluette/Model/Notification.php
@@ -1,6 +1,8 @@
 <?php
 namespace Esperluette\Model;
 
+use Fwk\Fwk;
+
 class Notification
 {
     public static $types = array(
@@ -10,19 +12,39 @@ class Notification
 
     public static function read()
     {
+        $output = '';
         // Get notification
-        
+        $notifications = Fwk::Session()->read('notifications');
+
         // Build html
-        
+        if (is_array($notifications)) {
+            foreach ($notifications as $notificationType => $notificationMessages) {
+                foreach ($notificationMessages as $currentMessage) {
+                    $output .= '<div style="border: solid 1px red">' . implode('<br/>', (array) $currentMessage) . '</div>';;
+                }
+            }
+        }
+
         // Erase (consume)
-        
+        Fwk::Session()->destroy('notifications');
+
+        return $output;
     }
 
     public static function write($type, $message)
     {
-        // Add notification to pool if type exists
         if (in_array($type, static::$types)) {
+            $currentSessionData = Fwk::Session()->read('notifications');
 
+            if (isset($currentSessionData[$type]) && is_array($currentSessionData[$type])) {
+                $newData = array_merge($currentSessionData[$type], (array) $message);
+            } else {
+
+                $newData = (array) $message;
+            }
+            
+            $currentSessionData[$type] = $newData;
+            Fwk::Session()->write('notifications', $currentSessionData);
         }
     }
 }
