f0f53bdc71a4d2f060575c30555e3f2784452fa7
Mathieu LESNIAK <mathieu@lesniak.fr>
2013-10-28 22:50:32 +0100

    [Upg] Helper on app mode

--- Fwk/App.php
+++ Fwk/App.php
@@ -5,10 +5,29 @@ use ErrorException;
 
 class App extends Service
 {
+    const DEVELOPMENT_MODE  = 'development';
+    const PRELIVE_MODE      = 'prelive';
+    const PRODUCTION_MODE   = 'production';
+
     protected $parametersList   = array(
         'root',
         'mode',
         'url',
         'locale'
         );
+
+    public function isDevelopment()
+    {
+        return self::DEVELOPMENT_MODE == $this->mode;
+    }
+
+    public function isPrelive()
+    {
+        return self::PRELIVE_MODE == $this->mode;
+    }
+
+    public function isProduction()
+    {
+        return self::PRODUCTION_MODE == $this->mode;
+    }
 }
